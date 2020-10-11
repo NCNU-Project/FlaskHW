c3189221a58ed1b0bf4bdfa892009f6ba511ccd8
¸ê¤u¨t ²ø¤~½å <s108321036@lilina.csie.ncnu.edu.tw>
2020-10-12 00:39:22 +0800

    hw2 redirect/to/other/website

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -16,3 +17,19 @@ def user(name):
 @app.route('/mul/<int:n>')
 def mul(n):
     return render_template('mul.html', n=range(1,n+1))
+
+@app.route('/url')
+def url():
+    urls=[
+    ('bbc', 'https://bbc.com'),
+    ('cnn', 'https://cnn.com'),
+    ('NCNU', 'https://www.doc.ncnu.edu.tw/ncnu/'),
+    ]
+    return render_template('url.html', urls=urls)
+
+@app.route('/redirect/<path:url>')
+def new_url(url):
+    ip = request.remote_addr
+    f = open('server_log.txt', 'a')
+    f.write("%s [%s] %s\n" % (ip, datetime.now(), url)) 
+    return redirect(url)
