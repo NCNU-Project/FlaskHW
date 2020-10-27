e538dd67694757ab0854279d4e40b2df76c3aa02
¸ê¤u¨t ²ø¤~½å <s108321036@lilina.csie.ncnu.edu.tw>
2020-10-27 17:41:37 +0800

    hw3 and hw4 forms in /birthdayWeb

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, request
-from datetime import datetime
+from datetime import datetime, date
 
 app = Flask(__name__)
 
@@ -33,3 +33,42 @@ def new_url(url):
     f = open('server_log.txt', 'a')
     f.write("%s [%s] %s\n" % (ip, datetime.now(), url)) 
     return redirect(url)
+
+@app.route('/birthday', methods=['GET','POST'])
+def birthday():
+    week_day_dict = {
+        0 : "Monday",
+        1 : "Tuesday",
+        2 : "Wednesday",
+        3 : "Tuesday",
+        4 : "Friday",
+        5 : "SaterDay",
+        6 : "Sunday",
+    }
+    yyyy = str(request.values['yyyy'])
+    mm = str(request.values['mm'])
+    dd = str(request.values['dd'])
+    if(len(mm) < 2 ):
+        mm = '0'+mm
+    if(len(dd) < 2 ):
+        dd = '0' + dd
+    dateString = "{}/{}/{} is {}".format(yyyy,mm,dd, week_day_dict[datetime(int(request.values['yyyy']),int( request.values['mm']),int(request.values['dd'])).weekday()])
+    return dateString
+
+@app.route('/birthdayWeb')
+def birthdayWeb():
+    return render_template('calender.html')
+
+@app.route('/birthday1', methods = ['POST', 'GET'])
+def birthday1():
+    week_day_dict = {
+        0 : "Monday",
+        1 : "Tuesday",
+        2 : "Wednesday",
+        3 : "Tuesday",
+        4 : "Friday",
+        5 : "SaterDay",
+        6 : "Sunday",
+    }
+    day = date.fromisoformat(request.values['date'])
+    return day.strftime('%Y/%m/%d')+' is '+ week_day_dict[day.weekday()]
